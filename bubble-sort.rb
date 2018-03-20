10b6866613dc0da41f2719f028bde9d201b37555
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-03-20 10:24:35 -0400

    finished bubble_sort_by

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -6,7 +6,7 @@ array = [7,6,12,4,3,45,1]
 # but to start them off with initial values of 0 and we ,
 # we give them default values
 
-def bubbleSort(array, indexL=0, indexR=1, swapped=false)
+def bubble_sort(array, indexL=0, indexR=1, swapped=false)
 
 # swapped = false  # given default in first line, same as for the indexes
 
@@ -24,7 +24,7 @@ def bubbleSort(array, indexL=0, indexR=1, swapped=false)
   # keep going be calling this recursively again until the right most value,
   # for one complete pass
   if indexR < array.length
-      bubbleSort(array, indexL, indexR, swapped)
+      bubble_sort(array, indexL, indexR, swapped)
 
   else    #indexR == array.length
   # check if already sorted,
@@ -42,11 +42,58 @@ def bubbleSort(array, indexL=0, indexR=1, swapped=false)
 
 
       # restart
-      bubbleSort(array)
+      bubble_sort(array)
     end
   end
 
 end
 
+p bubble_sort(array)
 
-bubbleSort(array)
+def bubble_sort_by(array, indexL=0, indexR=1, swapped=false, &block)
+
+  # swapped = false  # given default in first line, same as for the indexes
+
+    # check if left > right (which is wrong)?
+    if block.call(array[indexL], array[indexR]) > 0
+      # wrong so swap them
+      array[indexL], array[indexR] = array[indexR], array[indexL]
+      swapped = true
+    end
+
+    indexL += 1
+    indexR += 1
+    # run function again
+    # keep going be calling this recursively again until the right most value,
+    # for one complete pass
+    if indexR < array.length
+        bubble_sort_by(array, indexL, indexR, swapped, &block)
+
+    else    #indexR == array.length
+    # check if already sorted,
+    # it is not if swapped == true
+    # if swapped == false, then done, return the array
+      if swapped == false
+        return array
+      else
+        # # reset the indexes:
+        # indexL = 0
+        # indexR = 1
+        # NO NEED TO RESET because if you don't pass anything,
+        # they will be set to their default values
+        # that we defined in the first line of the function
+
+
+        # restart
+        bubble_sort_by(array, &block)
+      end
+    end
+
+end
+
+# Blocks must be passed to methods outside of the argument parentheses
+array_sorted = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  right.length - left.length
+end
+
+p array_sorted
