7cf155baea90bc340a30690a9bb9d4bbe401e08c
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-03-20 11:37:32 -0400

    more enumerable method (any?, all?, none?, count)

--- advanced-building-blocks/my_enumerables.rb
+++ advanced-building-blocks/my_enumerables.rb
@@ -21,6 +21,55 @@ module Enumerable
      return new_array
    end
 
+   def my_all?
+     all = true
+     # Runs through each element to check if any DO NOT meet condition.
+     self.my_each do |element|
+       if !yield(element)
+         all = false
+         break # No need to continue after a false is found.
+       end
+     end
+     all  # Implicit return
+   end
+
+   def my_any?
+     any = false
+     # Runs through each element to check if any meet condition.
+     self.my_each do |element|
+       if yield(element)
+         any = true
+         break # No need to continue after a true is found.
+       end
+     end
+     any  # Implicit return
+   end
+
+   def my_none1? #NOTE: This method is significantly slower than my_none2?
+     !self.my_any? {yield}
+   end
+   def my_none2?
+     none = true
+     # Runs through each element to check if any meet condition.
+     self.my_each do |element|
+       if yield(element)
+         none = false
+         break # No need to continue after a true is found.
+       end
+     end
+     none  # Implicit return
+   end
+
+   def my_count
+     count = 0
+     self.my_each { |element| count +=1 if yield(element) }
+     count
+   end
+=begin
+   Create #my_count
+   Create #my_map
+   Create #my_inject
+=end
 
  end
 
@@ -28,5 +77,40 @@ array = ["Hey", "Hello", "Hurray", "SSR", 42]
 
 # array.my_each { |x| print "#{x} " }
 # array.my_each_with_index { |element, index| print "'#{element}' is at #{index}, " }
-print array.my_select { |element| element.to_s.length > 3 }
-print "\n"
+#print array.my_select { |element| element.to_s.length > 3 }
+# print array.my_all? {|e| e.to_s.length < 3}
+# print array.all? {|e| e.to_s.length < 3}
+# print "\n"
+# print array.my_all? {|e| e.to_s.length > 1}
+# print array.all? {|e| e.to_s.length > 1}
+# print "\n"
+
+# print array.my_any? {|e| e.to_s.length > 1}
+# print array.any? {|e| e.to_s.length > 1}
+# print "\n"
+# print array.my_any? {|e| e.to_s.length > 7}
+# print array.any? {|e| e.to_s.length > 7}
+# print "\n"
+
+# start1 = Time.now
+# 10000.times do
+#   array.my_none1? {|e| e.to_s.length > 7}
+#   array.my_none1? {|e| e.to_s.length > 4}
+# end
+# end1 = Time.now
+# puts "None1 took #{end1 - start1}."
+#
+# start2 = Time.now
+# 10000.times do
+#   array.my_none2? {|e| e.to_s.length > 7}
+#   array.my_none2? {|e| e.to_s.length > 4}
+# end
+# end2 = Time.now
+# puts "None2 took #{end2 - start2}."
+#
+# None1 took 0.032061421.
+# None2 took 0.018276347.
+#
+# print array.my_count {|e| e.to_s.length > 1}
+# print array.count {|e| e.to_s.length > 1}
+# print "\n"
